3a13e57846db40005cb4b43549fe5443aae22444
Owais28 <owaisathar2882@gmail.com>
2022-05-29 21:46:09 +0530

    update

--- main.js
+++ main.js
@@ -74,50 +74,52 @@ const makeNotesList = (notes=[{title:"Dummy Title",body:'loremsf sf fjdasloiea e
     noteLeftNotes.innerHTML = noteList.join('');
 
 
-    const deletNote = () => {
+    const deleteNote = () => {
         // adding event listeners to 
-    const addEventListenerToDeleteButtons = () => {
-        const deleteButtons = document.querySelectorAll('.fa-trash-can')
+        const addEventListenerToDeleteButtons = () => {
+            const deleteButtons = document.querySelectorAll('.fa-trash-can')
 
-        deleteButtons.forEach((deleteButton)=> {
+            deleteButtons.forEach((deleteButton)=> {
 
-            deleteButton.addEventListener('click', (event)=> {
-                let target = event.target
-                const superParent = target.parentNode.parentNode.parentNode;
+                deleteButton.addEventListener('click', (event)=> {
+                    let target = event.target
+                    const superParent = target.parentNode.parentNode.parentNode;
 
-                // testing...
-                // console.log(superParent.children[0].textContent)
+                    // testing...
+                    // console.log(superParent.children[0].textContent)
 
-                const targetTitle = superParent.children[0].textContent;
-                const targetBody = superParent.children[1].textContent;
+                    const targetTitle = superParent.children[0].textContent;
+                    const targetBody = superParent.children[1].textContent;
 
-                const localStorageTemporary = JSON.parse(localStorage.getItem('noteapp-notes'));
-                // console.log(localStorageTemporary)
+                    const localStorageTemporary = JSON.parse(localStorage.getItem('noteapp-notes'));
+                    // console.log(localStorageTemporary)
 
-                const newArrayOfNotesAfterFilter = localStorageTemporary.filter((note) => {
-                    if(note.title !== targetTitle && note.body !== targetBody){
-                        return true;
+                    const newArrayOfNotesAfterFilter = localStorageTemporary.filter((note) => {
+                        if(note.title !== targetTitle && note.body !== targetBody){
+                            return true;
+                        }
+                    })
+
+                    if(confirm('Are you sure to delete this note?') === true){
+                        localStorage.setItem('noteapp-notes',JSON.stringify(newArrayOfNotesAfterFilter))
                     }
+                    console.log(newArrayOfNotesAfterFilter);
+
+                    makeNotesList(newArrayOfNotesAfterFilter)
+
                 })
-
-                if(confirm('Are you sure to delete this note?') === true){
-                    localStorage.setItem('noteapp-notes',JSON.stringify(newArrayOfNotesAfterFilter))
-                }
-                console.log(newArrayOfNotesAfterFilter);
-
-                makeNotesList(newArrayOfNotesAfterFilter)
-
             })
-        })
 
-        // deleteButtons.
-        console.log(deleteButtons)
-    }
+            // deleteButtons.
+            console.log(deleteButtons)
+        }
+
+    addEventListenerToDeleteButtons()
 
     }
     
 
-    deletNote();
+    deleteNote();
 }
 
 makeNotesList(JSON.parse(localStorage.getItem('noteapp-notes')));
