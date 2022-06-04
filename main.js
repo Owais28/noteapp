efda480346b8d290f4d4dcc08fa6ef866d067b66
Owais28 <owaisathar2882@gmail.com>
2022-06-04 21:07:07 +0530

    added more options

--- main.js
+++ main.js
@@ -26,9 +26,9 @@ const noteBox = `<input type='text' name='noteTitle' id='' class='note__right-ti
 <div class='setting__menu-item add__todo'>
   <i class="fa-solid fa-square-check"></i><div class='font-choice'>Add Todo</div></div>
 <div class='setting__menu-item'>
-<i class="fa-solid fa-link-simple"></i><div class='font-choice'>Add link</div></div>
+  <i class="fa-solid fa-link"></i><div class='font-choice'>Add link</div></div>
 <div class='setting__menu-item'>
-  <div class='font-choice'>Add priority</div></div>
+  <i class="fa-solid fa-arrow-up-1-9"></i><div class='font-choice'>Add priority</div></div>
 </div>`;
 
 const modalButton = document.querySelector(".modal-button");
